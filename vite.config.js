5f274e95875d75ed36a5237a61a65b27daeff268
sarah001160 <sarah001160@gmail.com>
2025-03-17 02:56:34 +0800

    更正:復原 -.d.ts是for TypeScript用的...

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,9 @@ export default defineConfig({
       imports: [
         'vue', 'vue-router' // 將會自動導入 Vue 3 中的 API，如 `ref`, `reactive`, `computed` 等，你也可以在這裡添加其他常用函式庫
       ],
-      dts: 'src/auto-imports.d.ts', //test:確保有生成
     }),
     Components({
       dirs: ['src/components'],
-      dts: 'src/components.d.ts', //test:確保有生成
     }),
   ],
 })
